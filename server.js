802d6e19ba0bf9f053f2a52bf70bdf6494c8e3bb
hight007 <hight_007@hotmail.com>
2020-11-20 23:10:33 +0700

    Start Use Git

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server is running on port : ${PORT}`)
